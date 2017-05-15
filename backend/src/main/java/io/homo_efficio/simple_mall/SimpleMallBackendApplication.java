94f1f839f8a277d7f42d6bc2ac9cffad99fec66e
Homo Efficio <homo.efficio@gmail.com>
2017-05-15 19:42:22 +0900

    Initial commit with Spring Boot

---
+++ backend/src/main/java/io/homo_efficio/simple_mall/SimpleMallBackendApplication.java
@@ -0,0 +1,12 @@
+package io.homo_efficio.simple_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMallBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMallBackendApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/io/homo_efficio/simple_mall/SimpleMallBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package io.homo_efficio.simple_mall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleMallBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
