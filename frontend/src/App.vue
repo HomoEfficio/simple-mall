415073123fee32eccd690d7ad12dd180fb42b50f
Homo Efficio <homo.efficio@gmail.com>
2017-05-16 09:33:12 +0900

    App.vue에 uid 체크 로직 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,52 +3,13 @@
     <br />
     <navbar></navbar>
     <br />
-    <router-view></router-view>
+    <div v-if="hasUID()">
+      <router-view></router-view>
+    </div>
+    <div v-else>
+      <h2>Add '?uid=UID_VALUE' at the end of the URL</h2>
+    </div>
     <br /><br />
-    <h4> Welcome to your Vue.js app </h4>
-    <hr />
-    <p>
-      To get a better understanding of how this boilerplate works, please check out
-      <a href="https://github.com/ssouron/vuestart/blob/master/README.md" target="_blank">its Readme file</a>
-      , and the original template detailed
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">documentation</a>
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/ssouron/vuestart" target="_blank">repository</a>.
-    </p>
-    <p>
-      Several external addins or libraries are implemented in this rig. You can checkout their pages to find documentation and related stuff :
-      <ul>
-        <li>
-          <b><a href="https://github.com/vuejs/vue-resource" target="_blank">Vue Resource</a></b>
-          : Http and Ajax requests from your Vue app.
-        </li>
-        <li>
-          <b><a href="https://github.com/vuejs/vue-router" target="_blank">Vue Router</a></b>
-          : Routing without page reloading.
-        </li>
-        <li>
-          <b><a href="https://github.com/vuejs/vuex/" target="_blank">VueX</a></b>
-          : State and event store management.
-        </li>
-        <li>
-          <b><a href="https://github.com/javisperez/vuetranslate" target="_blank">Vue Translate</a></b>
-          : Internationalisation plugin.
-        </li>
-        <li>
-          <b><a href="http://getbootstrap.com/" target="_blank">Bootstrap</a></b>
-          : SASS version of the styling framework, including components.
-        </li>
-        <li>
-          <b><a href="http://fontawesome.io/" target="_blank">Font Awesome</a></b>
-          : SASS version of the icon provider.
-        </li>
-      </ul>
-    </p>
-    <hr />
-    <h4> Build an amazing application ! </h4>
   </div>
 </template>
 
@@ -56,7 +17,27 @@
 import Navbar from './components/Navbar'
 
 export default {
-  components: { Navbar }
+  components: { Navbar },
+  methods: {
+    hasUID() {
+        return this.uid;
+    }
+  },
+  data() {
+      return {
+          uid: (function() {
+            var url = document.location.href;
+            var paramString = url.substring(url.indexOf('?') + 1);
+            var params = paramString.split('&');
+            for (var i = 0, len = params.length; i < len; i++) {
+              var param = params[i];
+              if (param.trim().indexOf('uid') === 0)
+                return param.substring(param.indexOf('=') + 1);
+            }
+            return '';
+          }())
+      }
+  }
 }
 </script>
 
