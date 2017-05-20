997e0a574c41d4ce516c5f66e92eed33fa2b1f02
Homo Efficio <homo.efficio@gmail.com>
2017-05-20 14:59:48 +0900

    Data Rest의 RestResourceRepository 가 만든 컨트롤러가 /api에 바인딩 되도록 설정

--- backend/src/main/resources/static/js/sign.js
+++ backend/src/main/resources/static/js/sign.js
@@ -1,14 +1,35 @@
 new Vue({
     el: '#app',
     data: {
-        id: ''
+        userName: '',
+        form: document.getElementById('sign-form'),
     },
     methods: {
-        onSignIn() {
+        onSignIn(endPoint) {
             // GET /sign-in/{memberName}
+            this.form.method = 'GET';
+            this.form.action = endPoint + '/' + this.userName;
+            this.form.submit();
+
+            // axios.get(endPoint + '/' + this.userName)
+            //     .then(res => {
+            //         console.log(res);
+            //         document.location.href = './main.html'
+            //     })
+            //     .catch(err => {
+            //         console.log(err);
+            //     });
         },
-        onSignUp() {
-            // POST /members/{memberId}
+        onSignUp(endPoint) {
+            // POST /members/{memberName}
+            axios.post(endPoint + '/' + this.userName)
+                .then(res => {
+                    console.log(res);
+                    this.onSignIn('/sign-in');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         },
     }
 });
